4765dd8ce0b3ce273f31e7612fdb5d1c21ba890d
Gleb <glebtolkacev08@gmail.com>
2024-10-08 14:40:59 +0800

    #6 последний коммит перед переходом на правильную систему использования гита,
     (спустя две неудачные попытки)оу хаха, а я уже сделал правила на то чтобы не дать себе проиграть, ладно тогда пытаемся сделать сразу красиво

--- src/app/employee/employee.service.ts
+++ src/app/employee/employee.service.ts
@@ -13,7 +13,6 @@ export class EmployeeService {
 
   getEmployeesList(): Observable<Employee[]>{
     return this.httpClient.get<Employee[]>(`${this.baseURL}employees`);
-    //return this.httpClient.get<Employee[]>(`employees`);
   }
 
 }
