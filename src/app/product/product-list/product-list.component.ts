8341f0cfa391d8bbb776ca54a58e71dc092f7863
Gleb <glebtolkacev08@gmail.com>
2025-01-09 12:44:58 +0800

    #8 а почему бы не делать по несколько коммитов к одной задаче? это повысит степень зелености так то

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,25 +1,23 @@
 import {Component, OnInit} from '@angular/core';
 import {Product} from "../product";
 import {ProductService} from "../product.service";
+import {AbstractBasicFunctions} from "../../BasicFunctionalityForClasses/abstract-basic-functions";
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent extends AbstractBasicFunctions<Product> implements OnInit {
 
-  products: Product[] | undefined;
 
-  constructor(private productService: ProductService) {}
-
-  private getProducts(){
-    this.productService.getEntityList().subscribe(data=>
-    this.products=data);
+  constructor(private productService: ProductService) {
+    super(productService);
   }
 
+
   ngOnInit(): void {
-    this.getProducts();
+    this.getEntity();
   }
 
 }
