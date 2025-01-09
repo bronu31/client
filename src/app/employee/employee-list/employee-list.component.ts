b4fe8930d43dff07dd1783195786261fe1549a9b
Gleb <glebtolkacev08@gmail.com>
2025-01-09 11:53:18 +0800

    #8 ура, полиморфизм и наследование работают прям пум пум пум круто круто

--- src/app/employee/employee-list/employee-list.component.ts
+++ src/app/employee/employee-list/employee-list.component.ts
@@ -1,28 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Employee} from "../employee";
 import {EmployeeService} from "../employee.service";
+import {AbstractBasicFunctions} from "../../BasicFunctionalityForClasses/abstract-basic-functions";
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent implements OnInit {
+export class EmployeeListComponent extends AbstractBasicFunctions<Employee> implements OnInit {
 
-  employees: Employee[] | undefined;
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private employeeService: EmployeeService) {
+    super(employeeService);
+  }
 
   ngOnInit(): void {
-    this.getEmployees();
+    this.getEntity();
   }
 
-  private getEmployees(){
-    this.employeeService.getEntityList().subscribe(data =>{
-      this.employees=data;
-    })
-  }
-   convertToRuDate(date?:Date){
+  convertToRuDate(date?: Date) {
     return this.employeeService.birtdateToRuDate(date);
   }
 
