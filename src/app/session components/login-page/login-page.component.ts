b59afe9e5e9c091418d435329dcc99d26cdbd141
Gleb <glebtolkacev08@gmail.com>
2025-01-29 20:10:12 +0800

    #логин/8
    создан компонент для регистарции

--- src/app/session components/login-page/login-page.component.ts
+++ src/app/session components/login-page/login-page.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
